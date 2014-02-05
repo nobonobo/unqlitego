e7945470e795a7da3b79a9a420de5671735b1d4a
nobonobo <irieda@gmail.com>
2014-02-05 20:44:00 +0900

    merge

--- unqlite.go
+++ unqlite.go
@@ -1,7 +1,8 @@
 package unqlitego
 
 /*
-#cgo CFLAGS: -DUNQLITE_ENABLE_THREADS=1 -Wno-unused-but-set-variable
+#cgo linux CFLAGS: -DUNQLITE_ENABLE_THREADS=1 -Wno-unused-but-set-variable
+#cgo darwin CFLAGS: -DUNQLITE_ENABLE_THREADS=1
 #include "./unqlite.h"
 #include <stdlib.h>
 */
